1473623f144888106cd74138b454542d21a3d432
jagatjyoti-01 <jagatjyotidash33@gmail.com>
2025-08-31 23:43:22 +0530

    microservices

---
+++ user-services/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app=express();
+
+const bodyParser=require('body-parser');
+const port=3000;
+
+
+app.use(bodyParser.json());
+app.get("/",(req,res)=>{
+    res.send("User Service is up and running");
+})
+
+app.listen(port,()=>{
+    console.log(`User Service is running on port ${port}`);
+})
